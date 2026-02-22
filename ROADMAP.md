111486ae99fbf46f76f68bd059ca95b46e2e4506
Ryan Nett <JNett96@gmail.com>
2026-02-22 13:06:22 -0800

    Add all multiplatform targets, and use jvm-only by default

--- ROADMAP.md
+++ ROADMAP.md
@@ -1,3 +1,6 @@
 Things I might add in the future:
 
+- Restructure generation. Full name not in name, move it to package. Changes will be caught by ABI dumps.
+- A way to create IR method call builders with all params, etc.
+- ExportParams for a method.
 - It's empty
--- gradle/build-logic/kotlin-setup/src/main/kotlin/build/kotlin-multiplatform.gradle.kts
+++ gradle/build-logic/kotlin-setup/src/main/kotlin/build/kotlin-multiplatform.gradle.kts
@@ -6,35 +6,57 @@ plugins {
     kotlin("multiplatform")
 }
 
+val onlyJvm = providers.systemProperty("symbol-export.onlyJvm").orNull?.lowercase() == "true"
+
 kotlin {
     jvmToolchain(Shared::toolchain)
     jvm()
-    js() {
-        browser()
-        nodejs()
-    }
-    @OptIn(ExperimentalWasmDsl::class)
-    wasmJs() {
-        d8()
-        nodejs()
-        browser()
-    }
-    @OptIn(ExperimentalWasmDsl::class)
-    wasmWasi() {
-        nodejs()
-    }
-    iosArm64()
-    iosX64()
-    mingwX64()
-    macosX64()
-    macosArm64()
-    linuxX64()
-    linuxArm64()
-
-    val commonMain by sourceSets.getting {
-        dependencies {
+    if (!onlyJvm) {
+        js() {
+            browser()
+            nodejs()
         }
+        @OptIn(ExperimentalWasmDsl::class)
+        wasmJs() {
+            d8()
+            nodejs()
+            browser()
+        }
+        @OptIn(ExperimentalWasmDsl::class)
+        wasmWasi() {
+            nodejs()
+        }
+
+        // Apple
+        macosX64()
+        macosArm64()
+        iosArm64()
+        iosX64()
+        iosSimulatorArm64()
+        watchosArm32()
+        watchosArm64()
+        watchosX64()
+        watchosSimulatorArm64()
+        watchosDeviceArm64()
+        tvosArm64()
+        tvosX64()
+        tvosSimulatorArm64()
+
+        // Linux
+        linuxX64()
+        linuxArm64()
+
+        // Windows
+        mingwX64()
+
+        // Android Native
+        androidNativeArm32()
+        androidNativeArm64()
+        androidNativeX86()
+        androidNativeX64()
     }
+
+    applyDefaultHierarchyTemplate()
 }
 
 tasks.withType<Test>().configureEach {
